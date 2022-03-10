044c86a9bec404d955e7bd2177e714216fa70f0c
Cameron Johnson <clj00004@mix.wvu.edu>
2022-03-10 18:00:53 -0500

    Added Router Links/Passwords/Ciphers/Info Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CiphersComponent } from './ciphers/ciphers.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { PasswordsComponent } from './passwords/passwords.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent 
+  },
+  {
+    path:'Passwords',component: PasswordsComponent
+  },
+
+  {
+    path:'Ciphers',component:CiphersComponent
+  },
+  {
+    path:'Info', component: InfoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
