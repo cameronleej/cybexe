362729cf320b60d740493172f9a8d15bcb08c3c2
Cameron Johnson <clj00004@mix.wvu.edu>
2022-04-27 21:47:48 -0400

    information functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,16 @@ export class HomeComponent implements OnInit {
   runTime!:number;
   recovered!:number;
 
+  malwareResponse!:string;
+  malLevelResponse!:string;
+  securityResponse!:string;
+
+  isVirus: string = `A Virus is an executable computer program that moves malicious code into the computer of its victims
+              , and spreads by repeating itself. These are one of the most common types of attacks, and preventing them
+              is difficult.`;
+  
+  
+
   constructor() { }
 
   ngOnInit(): void {
@@ -50,12 +60,48 @@ export class HomeComponent implements OnInit {
     console.log("percent of infected devices that recovered: " + results[3]);
 
 
-    //setting the variables to the corresponding values
+    //setting the variables to the corresponding values for the front end
     this.devicesInfected = results[0];
     this.networkComp = results[1];
     this.runTime = results[2];
     this.recovered = results[3];
+
+
+    //this is used to print out the given parameters
+    this.malwareType = malwareType;
+    this.malwareResponse = this.printMalware(malwareType);
+
+    this.malwareLevel = malwareLevel;
+
+
     return results;
+  }
+
+  //this method is called to set the value of the malware 
+  //response in the information secion of the simulation
+  printMalware(type:string):string{
+    type = type.toLowerCase();
+    switch(type){
+      case "virus":
+        return `A Virus is an executable computer program that moves malicious code into the computer of its victims
+        , and spreads by repeating itself. These are one of the most common types of attacks, and preventing them
+        is difficult.`;
+      case "trojan":
+        return `A Trojan is a type of malware that looks like something familiar or trustworthy that, when clicked or executed,
+                it will steal information, disrupt, or even destroy your computer.`
+      case "ransomware":
+        return `Ransomware is a type of malware that when executed stops, freezes, or blocks off access
+                to your computer until money or something value to the hacker, is given up. This is 
+                special because of the hackers intentions, sometimes they have different motives than just 
+                breaking the device.`
+      case "worm":
+        return `A Worm in computer terms is a type of Trojan Horse that replicates itself to step through a 
+                computer or systems network. These types of malware are used to steal data, remove usage of hard drive space, 
+                and overpower networks so that they cannot be used correctly. `
+      default:
+        return "A Malware Type was not given..."
+
+    }
   }
 
   
